908812afec4f9a6b35a146be36d57a2fc7a42397
myflying <512710257@qq.com>
2018-05-02 17:30:26 +0800

    init gif make project

---
+++ pages/gifmake/gifmake.js
@@ -0,0 +1,78 @@
+// pages/gifmake/gifmake.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    gifinfo: {
+      'id': '1',
+      'gif_title': '为所欲为',
+      'gif_img_url': '../images/g2.jpg',
+      data: [
+        { 'hitvalue': '我就是饿死' },
+        { 'hitvalue': '死外边，从这里跳下去' },
+        { 'hitvalue': '也不会吃你们一点东西' },
+        { 'hitvalue': '真香' }
+      ]
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
