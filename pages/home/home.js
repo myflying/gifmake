6d7cfd36618517eff5f30709a0a151369a098782
myflying <512710257@qq.com>
2018-05-07 10:24:17 +0800

    fix bug

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,15 +1,17 @@
 // pages/home/home.js
-var list
+var list = null
 var page
 var pSize = 50
 var end = false
+var nodata = false
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     is_load_more: false,
-    is_end: end
+    is_end: end,
+    is_nodata: nodata
   },
 
   /**
@@ -19,13 +21,16 @@ Page({
     wx.setNavigationBarTitle({
       title: 'GIF在线制作',
     })
-
+    
     page = 1;
     list = null;
     var Page$this = this;
     this.getData(Page$this);
   },
   getData: function (that) {
+
+    console.log('getData--->')
+
     wx.showLoading({
       title: '加载中',
     })
@@ -39,20 +44,27 @@ Page({
       success: function (res) {
         wx.hideLoading()
         wx.stopPullDownRefresh();
-        
+
         if(page == 1){
           list = res.data.data;
         }else{
-          list = list.concat(res.data.data);
+          if(list != null){
+            list = list.concat(res.data.data);
+          }
         }
 
-        if (res.data.data.length < pSize){
+        if (res.data.data != null && res.data.data.length < pSize){
           end = true;
         }
-
+        
+        if(list == null || list.length == 0){
+          nodata = true;
+        }
+        
         that.setData({
           giflist: list,
-          is_end : end
+          is_end : end,
+          is_nodata : nodata
         });
       },
       fail: function (res) {
@@ -61,34 +73,12 @@ Page({
       }
     })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  onrefresh:function(){
+    page = 1;
+    list = null;
+    var Page$this = this;
+    this.getData(Page$this);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
