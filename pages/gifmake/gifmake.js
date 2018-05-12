79f1bd47745bd36938cac957ccab58dc4a75b6fa
myflying <512710257@qq.com>
2018-05-12 11:20:22 +0800

    send versionV2.0

--- pages/gifmake/gifmake.js
+++ pages/gifmake/gifmake.js
@@ -1,20 +1,38 @@
 // pages/gifmake/gifmake.js
+//获取应用实例
+const app = getApp()
 var gifinfo;
 var talklist;
 var oldList;
 var id;
+var code;
+var encryptedData;
+var iv;
+var isUse = false;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    showModal: false,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        code = res.code
+      }
+    })
     wx.showLoading({
       title: '加载中',
     })
@@ -35,11 +53,13 @@ Page({
           title: gifinfo.name,
         })
         talklist = gifinfo.input_placeholder;
+        isUse = gifinfo.is_lock;
         oldList = [].concat(talklist);
         Page$this.setData({
           gifImgUrl: gifinfo.preview_image,
           senslist: talklist,
-          maxlength: gifinfo.zimu_num
+          maxlength: gifinfo.zimu_num,
+          nickname:decodeURI(gifinfo.nickname)
         })
       },
       fail: function (res) {
@@ -47,56 +67,39 @@ Page({
         console.log('fail--->')
       }
     })
-
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  loadUserInfo: function (e) {
+    var Page$this = this;
+    
+    var lock = true;
+    console.log(typeof wx.getStorageSync('is_lock'))
 
-  },
+    if (wx.getStorageSync('is_lock') == '' && typeof wx.getStorageSync('is_lock') == 'string') {
+      lock = true
+    } else {
+      lock = wx.getStorageSync('is_lock')
+    }
+    console.log('make--->' + lock)
+    if(isUse == 1){
+      if (lock) {
+        wx.getUserInfo({
+          success: function (res) {
+            Page$this.data.userinfo = res.userInfo
+            console.log(res.userInfo)
+            encryptedData = res.encryptedData;
+            iv = res.iv;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+            //获取用户信息之后登录
+            Page$this.userLogin(Page$this);
+          }
+        })
+      }else{
+        this.create();
+      }
+    }else{
+      this.create();
+    }
   },
 
   inputChange:function(e){
@@ -109,8 +112,9 @@ Page({
       talklist[i] = oldinput;
     }
   },
-  create:function(){
 
+  //制作
+  create:function(){
     var params = talklist.toString();
     params = params.replace(/,/g,'%#');
 
@@ -149,6 +153,104 @@ Page({
         console.log("fail--->" + JSON.stringify(res));
       }
     })
+  },
 
+  /**
+     * 弹出框蒙层截断touchmove事件
+     */
+  preventTouchMove: function () {
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function () {
+    this.setData({
+      showModal: false
+    });
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.hideModal();
+  },
+
+ /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    var Page$this = this;
+    return {
+      title: 'gif在线制作',
+      path: '/pages/home/home',
+      imageUrl: '',
+      success: function (res) {
+        wx.showToast({
+          title: '分享成功',
+        })
+        // 转发成功
+        console.log('转发成功')
+
+        wx.setStorage({
+          key: "is_lock",
+          data: false
+        })
+
+        Page$this.setData({
+          showModal: false
+        })
+        Page$this.userLogin(Page$this);
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log('转发失败')
+        wx.showToast({
+          title: '分享失败',
+        })
+      }
+    }
+  },
+
+  userLogin:function(that){
+    console.log(code + '---' + encryptedData + '---' + iv)
+    wx.request({
+      url: 'https://nz.qqtn.com/zbsq/index.php?m=api&c=make_gif&a=init',
+      //注册
+      data: {
+        'code': code,
+        'encryptedData': encryptedData,
+        'iv': iv,
+      },
+      method: 'GET',
+      success: function (result) {
+        console.log(result.data)
+        if(result && result.data){
+          if(result.data.code == 1){
+            console.log("登录成功")
+            if (result.data.data.is_share == 1){
+              wx.setStorage({
+                key: "is_lock",
+                data: false
+              })
+
+              that.create();
+            }else{
+              that.setData({
+                showModal: true
+              })
+            }
+
+          }else{
+            console.log("登录失败")
+          }
+        }else{
+          console.log("登录失败")
+        }
+      },
+      fail: function (res) {
+        console.log("登录失败")
+      },
+    })
   }
+
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,12 @@
 // pages/home/home.js
+//获取应用实例
+const app = getApp()
 var list = null
 var page
 var pSize = 50
 var end = false
 var nodata = false
+var lock = true;
 Page({
   /**
    * 页面的初始数据
@@ -11,7 +14,8 @@ Page({
   data: {
     is_load_more: false,
     is_end: end,
-    is_nodata: nodata
+    is_nodata: nodata,
+    is_lock :true
   },
 
   /**
@@ -22,11 +26,22 @@ Page({
       title: 'GIF在线制作',
     })
     
+    if(wx.getStorageSync('is_lock') == '' && typeof wx.getStorageSync('is_lock') == 'string'){
+      lock = true
+    }else{
+      lock = wx.getStorageSync('is_lock')
+    }
+
+    console.log('home--->' + lock)
+    this.setData({
+      is_lock:lock
+    })
     page = 1;
     list = null;
     var Page$this = this;
     this.getData(Page$this);
   },
+
   getData: function (that) {
 
     console.log('getData--->')
@@ -123,6 +138,45 @@ Page({
     console.log('id--->'+id);
     wx.navigateTo({
       url: '/pages/gifmake/gifmake?id='+id,
+    })
+  },
+  
+  version: function () {
+    var text = 'GIF在线制作提醒您：GIF在线制作所有内容都由网友自行发布提供,GIF' +
+      '在线制作仅为网友提供信息的交流平台。根据用户的信息发布指令' +
+      'F在线制作系统会以非人工方式自动生成GIF。GIF在线制作自身不控' +
+      '制、编辑或修改任何网页的信息。GIF在线制作重视知识产权保护' +
+      '并制定了旨在保护权利人的合法权益的措施和步骤, 当权利人发现' +
+      '在GIF在线制作的内容侵犯其著作权时, 请权利人向GIF在线制作发出书' +
+      '面“权利通知”,GIF在线制作将依法采取措施移除相关内容或断开相关' +
+      '链接。GlF在线制作高度重视知识产权保护并遵守各项知识产权法律' +
+      '法规和具有约束力的规范性文件。根据法律、法规和规范性文件要' +
+      '求, GIF在线制作制定了旨在保护知识产权权利人合法权益的措施和' +
+      '步骤, 当著作权人或依法可以行使著作权的权利人(以下简称“权利' +
+      '人)发现在GIF在线制作网页的内容侵犯其著作权时, 权利人应事先' +
+      '向GIF在线制作发出书面的“权利通知”, GIF在线制作将根据法律法规和' +
+      '政府规范性文件采取措施移除相关内容或断开相关链接。具体措施' +
+      '和步骤如下:权利通知任何个人或单位如果同时符合以下两个条' +
+      '件:1、是某一作品的著作权人或依法可以行使著作权的权利人' +
+      '2、通过用户上传信息指令以非人工方式自动生成的GIF侵犯了上述' +
+      '作品的著作权。请上述个人或单位务必以书面的通讯方式向GIF字' +
+      '幕菌提交权利通知。为有效处理上述个人或单位的权利通知, 通知' +
+      '书应当包含下列内容:1、权利人的姓名(名称) 、联系方式和地址' +
+      '2、要求删除或者断开链接的侵权作品、表演、录像制品的名称和网' +
+      '络地址;3、构成侵权的初步证明材料(包括且不限于权利人对涉嫌' +
+      '侵权内容拥有著作权或依法可以行使著作权的权属证明) 。4、权利' +
+      '人对通知书的真实性声明。5、请您签署该文件, 如果您是依法成立' +
+      '的机构或组织, 请您加盖公章。请您把以上资料和联络方式书面发' +
+      '往以下地址: gifmake2018_online@163.com'
+    wx.showModal({
+      title: '免责申明',
+      content: text,
+      showCancel: false,
+      success: function (res) {
+        if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
     })
   }
 })
